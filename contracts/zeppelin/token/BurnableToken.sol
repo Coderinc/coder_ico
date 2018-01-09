8b4eb89acaed789af1d193524fe20903a6118d43
Pavel Rubin <pash7ka@gmail.com>
2018-01-09 06:19:09 +0300

    Update OpenZeppelin to fix compilation warnings

--- contracts/zeppelin/token/BurnableToken.sol
+++ contracts/zeppelin/token/BurnableToken.sol
@@ -1,13 +1,12 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.18;
 
-import './StandardToken.sol';
-import '../ownership/Ownable.sol';
+import './BasicToken.sol';
 
 /**
  * @title Burnable Token
  * @dev Token that can be irreversibly burned (destroyed).
  */
-contract BurnableToken is StandardToken, Ownable {
+contract BurnableToken is BasicToken {
 
     event Burn(address indexed burner, uint256 value);
 
@@ -15,9 +14,7 @@ contract BurnableToken is StandardToken, Ownable {
      * @dev Burns a specific amount of tokens.
      * @param _value The amount of token to be burned.
      */
-    function burn(uint256 _value) onlyOwner public {
-        require(_value > 0);
-
+    function burn(uint256 _value) public {
         require(_value <= balances[msg.sender]);
         // no need to require value <= totalSupply, since that would imply the
         // sender's balance is greater than the totalSupply, which *should* be an assertion failure
