1e43a8c9af8fb2b03bb0d858e19e83890b9718a2
Pavel Rubin <pash7ka@gmail.com>
2018-01-09 06:24:30 +0300

    Fix compilation warnings

--- contracts/CoderCoin.sol
+++ contracts/CoderCoin.sol
@@ -20,11 +20,11 @@ contract CoderCoin is MintableToken, BurnableToken, HasNoContracts, HasNoTokens 
         _;
     }
     
-    function transfer(address _to, uint256 _value) canTransfer returns (bool) {
+    function transfer(address _to, uint256 _value) canTransfer public returns (bool) {
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) canTransfer returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) canTransfer public returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
 
